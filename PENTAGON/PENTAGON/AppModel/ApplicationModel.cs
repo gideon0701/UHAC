90989d94e56c7d4860c26658739735bd3141930f
gideon0701 <gideon.050196@gmail.com>
2017-11-15 02:31:54 +0800

    This is our main code POWERR!

---
+++ PENTAGON/PENTAGON/AppModel/ApplicationModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PENTAGON.Models;
+
+namespace PENTAGON.AppModel
+{
+    public class ApplicationModel
+    {
+        private myhealthDbEntities db = new myhealthDbEntities();
+
+        // To check if the employee email and password match
+        public employee getAccess(employee employee) 
+        {
+           
+            var res = db.employees
+                .Where(e => e.email == employee.email && e.pwd == employee.pwd)
+                .FirstOrDefault();
+
+            return res;
+        }
+
+    }
+}
---
+++ PENTAGON/PENTAGON/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using PENTAGON.Models;
+using PENTAGON.AppModel;
+namespace PENTAGON.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationModel appmodel;
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //POST: Home/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include ="email,pwd")] employee employees)
+        {
+            appmodel = new ApplicationModel();
+            if (appmodel.getAccess(employees) != null)
+            {
+                var emp = appmodel.getAccess(employees);
+                Session["Emp_ID"] = emp.id;
+                return RedirectToAction("Index","EmployeeDashboard",new { id = emp.id});
+            }
+            employees.Login_Message = "Wrong Email or Password";
+            return View("Index", employees);
+        }
+    }
+}
